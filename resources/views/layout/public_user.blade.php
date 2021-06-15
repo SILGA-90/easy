bca75e7eeb47483fcc3e4d73fc676b791ad6bd98
SILGA <kingsoulpower@gmail.com>
2021-06-15 23:03:43 +0000

    Update database connection

--- resources/views/layout/public_user.blade.php
+++ resources/views/layout/public_user.blade.php
@@ -35,7 +35,7 @@
     </div>
 
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 
